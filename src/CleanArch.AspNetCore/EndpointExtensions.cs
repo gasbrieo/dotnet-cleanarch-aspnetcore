c6585d81a0da49ce46ec57ef35b9b566d3dbadb3
Gabriel <gaberabreu.tech@gmail.com>
2025-07-22 16:03:06 -0300

    feat: endpoint to be versioned by the user creating specific interface for each version (#4)

--- src/CleanArch.AspNetCore/EndpointExtensions.cs
+++ src/CleanArch.AspNetCore/EndpointExtensions.cs
@@ -7,19 +7,33 @@ public static class EndpointExtensions
         var endpoints = assembly
             .DefinedTypes
             .Where(t => t is { IsAbstract: false, IsInterface: false } && typeof(IEndpoint).IsAssignableFrom(t))
-            .Select(t => ServiceDescriptor.Transient(typeof(IEndpoint), t));
+            .SelectMany(t =>
+            {
+                var interfaces = t.GetInterfaces()
+                    .Where(i => typeof(IEndpoint).IsAssignableFrom(i) && i != typeof(IEndpoint));
+                return interfaces.Select(i => ServiceDescriptor.Transient(i, t));
+            });
 
         services.TryAddEnumerable(endpoints);
 
         return services;
     }
 
-    public static IApplicationBuilder MapEndpoints(this WebApplication app)
+    public static IApplicationBuilder MapEndpoints<TEndpoint>(this WebApplication app, ApiVersion version) where TEndpoint : IEndpoint
     {
-        var endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+        var versionSet = app.NewApiVersionSet()
+            .HasApiVersion(version)
+            .ReportApiVersions()
+            .Build();
+
+        var group = app.MapGroup("api/v{version:apiVersion}")
+            .WithApiVersionSet(versionSet)
+            .HasApiVersion(version);
+
+        var endpoints = app.Services.GetRequiredService<IEnumerable<TEndpoint>>();
 
         foreach (var endpoint in endpoints)
-            endpoint.MapEndpoint(app);
+            endpoint.MapEndpoint(group);
 
         return app;
     }
--- src/CleanArch.AspNetCore/GlobalUsings.cs
+++ src/CleanArch.AspNetCore/GlobalUsings.cs
@@ -1,4 +1,6 @@
+global using System;
 global using System.Reflection;
+global using Asp.Versioning;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Routing;
