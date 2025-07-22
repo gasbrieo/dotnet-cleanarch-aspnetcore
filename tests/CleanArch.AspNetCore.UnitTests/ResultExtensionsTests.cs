388ab6be0f3cb8b95f717dc53afc43718110aa02
Gabriel <gaberabreu.tech@gmail.com>
2025-07-22 12:43:27 -0300

    feat: add result extensions (#3)
    * feat: add result extensions
    
    * test: fix test setup to run correctly

---
+++ tests/CleanArch.AspNetCore.UnitTests/ResultExtensionsTests.cs
@@ -0,0 +1,88 @@
+namespace CleanArch.AspNetCore.UnitTests;
+
+public class ResultExtensionsTests
+{
+    [Fact]
+    public void Match_WhenIsSuccess_ShouldCallOnSuccess()
+    {
+        // Arrange
+        var successCallback = new Mock<Func<bool>>();
+        successCallback.Setup(c => c()).Returns(true);
+
+        var failureCallback = new Mock<Func<Result, bool>>();
+        failureCallback.Setup(c => c(It.IsAny<Result>())).Returns(false);
+
+        var result = Result.Success();
+
+        // Act
+        var output = result.Match(successCallback.Object, failureCallback.Object);
+
+        // Assert
+        Assert.True(output);
+        successCallback.Verify(c => c(), Times.Once);
+        failureCallback.Verify(c => c(It.IsAny<Result>()), Times.Never);
+    }
+
+    [Fact]
+    public void Match_WhenIsSuccessWithValue_ShouldCallOnSuccess()
+    {
+        // Arrange
+        var successCallback = new Mock<Func<int, bool>>();
+        successCallback.Setup(c => c(It.IsAny<int>())).Returns(true);
+
+        var failureCallback = new Mock<Func<Result, bool>>();
+        failureCallback.Setup(c => c(It.IsAny<Result>())).Returns(false);
+
+        var result = Result.Success(1);
+
+        // Act
+        var output = result.Match(successCallback.Object, failureCallback.Object);
+
+        // Assert
+        Assert.True(output);
+        successCallback.Verify(c => c(result.Value), Times.Once);
+        failureCallback.Verify(c => c(It.IsAny<Result>()), Times.Never);
+    }
+
+    [Fact]
+    public void Match_WhenIsFailure_ShouldCallOnFailure()
+    {
+        // Arrange
+        var successCallback = new Mock<Func<bool>>();
+        successCallback.Setup(c => c()).Returns(true);
+
+        var failureCallback = new Mock<Func<Result, bool>>();
+        failureCallback.Setup(c => c(It.IsAny<Result>())).Returns(false);
+
+        var result = Result.Failure(new Error("ErrorCode", "ErrorDescription", ErrorType.Failure));
+
+        // Act
+        var output = result.Match(successCallback.Object, failureCallback.Object);
+
+        // Assert
+        Assert.False(output);
+        failureCallback.Verify(c => c(result), Times.Once);
+        successCallback.Verify(c => c(), Times.Never);
+    }
+
+    [Fact]
+    public void Match_WhenIsFailureWithValue_ShouldCallOnFailure()
+    {
+        // Arrange
+        var successCallback = new Mock<Func<int, bool>>();
+        successCallback.Setup(c => c(It.IsAny<int>())).Returns(true);
+
+        var failureCallback = new Mock<Func<Result<int>, bool>>();
+        failureCallback.Setup(c => c(It.IsAny<Result<int>>())).Returns(false);
+
+        var result = new Result<int>(1, false, Error.NullValue);
+
+        // Act
+        var output = result.Match(successCallback.Object, failureCallback.Object);
+
+        // Assert
+        Assert.False(output);
+        failureCallback.Verify(c => c(result), Times.Once);
+        successCallback.Verify(c => c(It.IsAny<int>()), Times.Never);
+    }
+}
